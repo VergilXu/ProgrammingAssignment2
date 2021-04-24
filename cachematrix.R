c1f4d3efed03fc80b0ce4b0f6de9b9a2cb0d3b3a
VergilXu <xsy3579@gmail.com>
2021-04-24 20:29:48 +0800

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -9,10 +9,18 @@
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
+                ## Using "<<-" to managing variables in different environment.
+                ## Double arrow operator can modify variables in parent levels.
+                ## when excute 'x <<- y', the variable x changed in the parent levels(makeCacheMatrix
+                ## enoironment.)
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
+        ## get() function using the principle of lexical scoping.
+        ## x is not defined in the argument ,it is called free variable.
+        ## so x is found in the environment where the function get() is defined (in this case, get() is defined in the /
+        ## makeCacheMatrix environment.
+        get <- function() x 
         setSolve <- function(solve) m <<- solve
         getSolve <- function() m
         list(set = set, get = get, 
@@ -21,17 +29,20 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve function invert a singular matrix created by "makeCacheMatrix" function.
+## cacheSolve function invert a singular matrix created by "c" function.
 ## x should be the object name of "makeCacheMatrix" function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## '$' is used to extract the element from a list.
         m <- x$getSolve()
+        ## 'm' should be null when the orginal matrix (created by makeCacheMatrix) haven't been calculated
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
+        ## solve() function computes the inverse of a square matrix
         m <- solve(data)
         x$setSolve(m)
         m
